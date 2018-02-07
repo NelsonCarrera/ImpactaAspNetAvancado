a7731477a58b0682e49c61a3d948b187c9dae31a
no1605 <no1605@4P16_05>
2018-02-07 20:55:46 -0200

    Commit Aula 20180207d2054h

---
+++ Loja.Mvc/Filters/ErrorLogFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace Loja.Mvc.Filters
+{
+    public class ErrorLogFilter : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.Exception != null) //Para evitar erro looping
+            {
+
+                var controller = filterContext.RouteData.Values["controller"].ToString();
+                var action = filterContext.RouteData.Values["action"].ToString();
+
+                var loggerName = $"{controller}Controler.{action}";
+
+                log4net.LogManager.GetLogger(loggerName).Error(filterContext.Exception.Message, filterContext.Exception);
+
+                base.OnException(filterContext); 
+            }
+        }
+    }
+}
